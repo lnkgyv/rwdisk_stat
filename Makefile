70f20a389174688a119d057851c06a3e402dbea4
Yurij Goncharuk <lnkgyv@gmail.com>
2015-04-17 14:56:07 +0300

    Fix Makefile compleated

--- Makefile
+++ Makefile
@@ -1,4 +1,29 @@
-all: rwdisk_stat
+CURR_DIG             = ./
+PROG_PATH            = /usr/bin/
+PROG_NAME            = rwdisk_stat
+DOC                  = README
+DOC_PATH             = /usr/share/rwdisk_stat/
+SERVICE              = rwdisk_stat.service
+SYSTEMD_SERVICE_PATH = /etc/systemd/system/
+PGCFG                = pgsql.conf
+PGCFG_PATH           = /etc/rwdisk_stat/
+
+default: rwdisk_stat
+
 rwdisk_stat: rwdisk_stat.c
 	cc -g -l pq -o rwdisk_stat rwdisk_stat.c params.c pgsql.c
 
+install: rwdisk_stat
+	cp $(PROG_NAME) $(PROG_PATH)
+	mkdir -p $(DOC_PATH)
+	mkdir -p $(PGCFG_PATH)
+	cp $(DOC) $(DOC_PATH)
+	cp $(PGCFG) $(PGCFG_PATH)$(PGCFG)
+	cp $(SERVICE) $(SYSTEMD_SERVICE_PATH)
+
+uninstall:
+	rm -f $(DOC_PATH)$(DOC)
+	rm -f $(SYSTEMD_SERVICE_PATH)$(SERVICE)
+	rm -f $(PROG_PATH)$(PROG_NAME)
+	rmdir $(DOC_PATH)
+	rmdir $(PGCFG_PATH)
--- params.h
+++ params.h
@@ -6,22 +6,22 @@
 
 #define MAX_PATH_LENGTH  255
 #define MAX_QUERY_LENGTH 255
-#define DEF_CFG_PATH     "/etc/adm/rwdisk_stat.conf"
+#define DEF_CFG_PATH     "/etc/rwdisk_stat/pgsql.conf"
 
-#define check_path_length(parm, max_length)                                                                                \
-             if(strlen(parm) > max_length){                                                                                \
-               fprintf(stderr, "Path "#parm" too long. Maybe something wrong?\n");                                         \
-               exit(EXIT_FAILURE);                                                                                         \
-             }                                                                                                             \
+#define check_path_length(parm, max_length)                                                                                  \
+             if(strlen(parm) > max_length){                                                                                  \
+               fprintf(stderr, "Path "#parm" too long. Maybe something wrong?\n");                                           \
+               exit(EXIT_FAILURE);                                                                                           \
+             }                                                                                                               \
 
-#define call_help()                                                                                                        \
-    fprintf(stderr, "\nUsage: %s [OPTION] ... \n", argv[0]);                                                               \
-    fprintf(stderr, "-c        Database connect parameters ('by default-if no set' equal /etc/adm/rwdisk_stat.conf) \n");  \
-    fprintf(stderr, "-d        Disk device (/dev/sdX, /dev/hdX) \n");                                                      \
-    fprintf(stderr, "-i        Interval (in seconds but not '0')\n");                                                      \
-    fprintf(stderr, "-D        Deamonize                        \n");                                                      \
-    fprintf(stderr, "-h        Print help. This message')\n\n");                                                           \
-    fprintf(stderr, "Example:\n %s -d /dev/sda -i 5\n\n", argv[0]);                                                        \
+#define call_help()                                                                                                          \
+    fprintf(stderr, "\nUsage: %s [OPTION] ... \n", argv[0]);                                                                 \
+    fprintf(stderr, "-c        Database connect parameters ('by default-if no set' equal /etc/rwdisk_stat/pgsql.conf) \n");  \
+    fprintf(stderr, "-d        Disk device (/dev/sdX, /dev/hdX) \n");                                                        \
+    fprintf(stderr, "-i        Interval (in seconds but not '0')\n");                                                        \
+    fprintf(stderr, "-D        Deamonize                        \n");                                                        \
+    fprintf(stderr, "-h        Print help. This message')\n\n");                                                             \
+    fprintf(stderr, "Example:\n %s -d /dev/sda -i 5\n\n", argv[0]);                                                          \
 
 /* deploy argumants for main*/
 void deploy_arguments(int argc, char *argv[], char *devpath, char *cfgpath, int *interval, char *Daemon);
